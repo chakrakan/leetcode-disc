b891f0137c8e4504eb60a90cdfc7fa18975aaca4
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-07-13 14:00:34 -0400

    up

--- bot.js
+++ bot.js
@@ -1,14 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const prefix = '!';
+
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-	if (msg.content === 'problem') {
+	console.log(msg.content);
+	if (msg.content.includes(`${prefix}problem`)) {
 		msg.reply('Here\'s a random leetcode problem for you:');
 	}
 });
 
-client.login(process.env.token);
+client.login(process.env.DISCORD_BOT_TOKEN);
